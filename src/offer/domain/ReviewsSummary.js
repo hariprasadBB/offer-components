e0c6807548b787ba379d5aacd969c473a019be84
SwathiYakkali <swathi.yakkali@bankbazaar.com>
2019-06-11 14:47:31 +0530

    Added prepare in ReviewsSummary

--- src/offer/domain/ReviewsSummary.js
+++ src/offer/domain/ReviewsSummary.js
@@ -1,27 +1,8 @@
+import ServiceProxy from "../../helpers/ServiceProxy";
+import Logger from "../../helpers/Logger";
+
 export default class ReviewsSummary {
-    static reviewsSummaryForAllCardsOrBanks = { //temp change. Will have make han ajax call and retrieve this data.
-        "10234": {
-            "avgRating":  {
-                "rating": 2.5,
-                "scale": 5.0
-            },
-            "count" : 6897
-        },
-        "10235": {
-            "avgRating":  {
-                "rating": 3.5,
-                "scale": 5.0
-            },
-            "count" : 10987
-        },
-        "10236": {
-            "avgRating":  {
-                "rating": 4.5,
-                "scale": 5.0
-            },
-            "count" : 979
-        }
-    };
+    static reviewsSummaryForAllCardsOrBanks;
 
     constructor(id, bankId) {
         this.reviewsSummary = ReviewsSummary.reviewsSummaryForAllCardsOrBanks[id || bankId];
@@ -37,5 +18,42 @@ export default class ReviewsSummary {
 
     getCount() {
         return this.reviewsSummary.count;
+    }
+
+    static prepare(context) {
+        const dummyResponse = { 
+            "10234": {
+                "avgRating":  {
+                    "rating": 2.5,
+                    "scale": 5.0
+                },
+                "count" : 6897
+            },
+            "10235": {
+                "avgRating":  {
+                    "rating": 3.5,
+                    "scale": 5.0
+                },
+                "count" : 10987
+            },
+            "10236": {
+                "avgRating":  {
+                    "rating": 4.5,
+                    "scale": 5.0
+                },
+                "count" : 979
+            }
+        };
+
+        const requestURL = "/getReviewsSummary.html?productType=" + context.getProductTypeLongCode();
+
+        const promise = ServiceProxy.getInstance().fetchJson(requestURL)
+            .then((response) => ReviewsSummary.reviewsSummaryForAllCardsOrBanks = JSON.parse(response))
+            .catch((e) => {
+                Logger.error(requestURL + " failed to fetch reviews : ", e);
+                ReviewsSummary.reviewsSummaryForAllCardsOrBanks = dummyResponse; //temp change till the endpoint is available.
+            });
+
+        return promise;
     }
 }
